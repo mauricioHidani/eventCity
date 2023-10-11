b5163d426963a30cf2dc1a5b84586f1edf5ba55d
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:24:41 -0300

    feat: middle point exception when resource not founded

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.EventDTO;
 import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.NotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,19 @@ public class EventService {
     private EventRepository repository;
 
     public EventDTO updateById(Long id, EventDTO event) {
-        Event founded = repository.getReferenceById(id);
+        try {
+            Event founded = repository.getReferenceById(id);
 
-        founded.setName(event.getName());
-        founded.setDate(event.getDate());
-        founded.setUrl(event.getUrl());
-        founded.getCity().setId(event.getCityId());
+            founded.setName(event.getName());
+            founded.setDate(event.getDate());
+            founded.setUrl(event.getUrl());
+            founded.getCity().setId(event.getCityId());
 
-        Event updated = repository.save(founded);
-        return new EventDTO(updated);
+            Event updated = repository.save(founded);
+            return new EventDTO(updated);
+        }
+        catch (EntityNotFoundException e) {
+            throw new NotFoundException("Event not founded");
+        }
     }
 }
