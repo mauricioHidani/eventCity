3ce523ba4b8329c596f12a8ddaa65c29e9a1cc3f
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:23:52 -0300

    feat: middle point exception when resource not founded

--- src/main/java/com/devsuperior/demo/controllers/handlers/ExceptionHandlerController.java
+++ src/main/java/com/devsuperior/demo/controllers/handlers/ExceptionHandlerController.java
@@ -1,8 +1,33 @@
 package com.devsuperior.demo.controllers.handlers;
 
+import com.devsuperior.demo.dto.ExceptionResponse;
+import com.devsuperior.demo.services.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Clock;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFound(NotFoundException e,
+                                                              HttpServletRequest req) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ExceptionResponse res = ExceptionResponse
+                .builder()
+                .timestamp(Instant.now(Clock.systemUTC()))
+                .status(status.value())
+                .error(e.getMessage())
+                .path(req.getRequestURI())
+                .build();
+
+        return ResponseEntity
+                .status(status)
+                .body(res);
+    }
 }
