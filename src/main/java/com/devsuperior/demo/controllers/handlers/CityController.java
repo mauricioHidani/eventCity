d0f5ee8f3259a91a615a516951a25c4a54f5465b
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:29:55 -0300

    feat: city find all

--- src/main/java/com/devsuperior/demo/controllers/handlers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/handlers/CityController.java
@@ -1,9 +1,14 @@
 package com.devsuperior.demo.controllers.handlers;
 
+import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
@@ -12,4 +17,10 @@ public class CityController {
     @Autowired
     private CityService service;
 
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity.ok(
+                service.findAll()
+        );
+    }
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 public class CityService {
@@ -10,4 +15,12 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(CityDTO::new)
+                .sorted(Comparator.comparing(CityDTO::getName))
+                .toList();
+    }
 }
