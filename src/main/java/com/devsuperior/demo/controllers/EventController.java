6e8b151baf78e9605417dc4ffba7501bd496f2eb
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:16:35 -0300

    feat: event update by id

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,7 +1,12 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.EventDTO;
 import com.devsuperior.demo.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ public class EventController {
 
     @Autowired
     private EventService service;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> updateById(@PathVariable Long id, @RequestBody EventDTO event) {
+        return ResponseEntity.ok(
+                service.updateById(id, event)
+        );
+    }
 }
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,16 @@ public class EventService {
 
     @Autowired
     private EventRepository repository;
+
+    public EventDTO updateById(Long id, EventDTO event) {
+        Event founded = repository.getReferenceById(id);
+
+        founded.setName(event.getName());
+        founded.setDate(event.getDate());
+        founded.setUrl(event.getUrl());
+        founded.getCity().setId(event.getCityId());
+
+        Event updated = repository.save(founded);
+        return new EventDTO(updated);
+    }
 }
