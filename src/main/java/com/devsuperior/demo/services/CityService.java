9e5ab764dbb1595c9200ef057156b2f770d0600f
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:41:09 -0300

    feat: exception when deleting a city with an event

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -3,8 +3,10 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
 import com.devsuperior.demo.services.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,7 +43,11 @@ public class CityService {
         if (!repository.existsById(id)) {
             throw new NotFoundException("City not founded");
         }
-
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Violation integraty data");
+        }
     }
 }
