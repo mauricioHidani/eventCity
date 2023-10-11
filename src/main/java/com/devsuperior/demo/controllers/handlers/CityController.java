107638d38927fb8390108bf50a95b7181f2a14fb
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:26:32 -0300

    feat: init city service

--- src/main/java/com/devsuperior/demo/controllers/handlers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/handlers/CityController.java
@@ -1,10 +1,15 @@
 package com.devsuperior.demo.controllers.handlers;
 
+import com.devsuperior.demo.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
+
+    @Autowired
+    private CityService service;
 
 }
