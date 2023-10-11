f92efadae9fd2a6b466cb30b92648faf0a22d29e
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-11 14:25:42 -0300

    feat: init city controller

---
+++ src/main/java/com/devsuperior/demo/controllers/handlers/CityController.java
@@ -0,0 +1,10 @@
+package com.devsuperior.demo.controllers.handlers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+}
